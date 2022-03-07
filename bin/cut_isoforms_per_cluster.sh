d920fbb58e30d13b745884c308d8b344849b198c
Anne Deslattes Mays, PhD <adeslat@scitechcon.org>
2022-03-07 10:38:54 -0500

    finally got the loop right to extract the fasta clusters

--- bin/cut_isoforms_per_cluster.sh
+++ bin/cut_isoforms_per_cluster.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
+#
+#  Filename:  cut_isoforms_per_cluster.sh
+#
+#  Setup:     Make a symbolic link to the directory containing the csv files for the ranks of the isoforms
+#             Put the files (copy or symbolic link)
+#                Alin_neg.flnc_BC.fasta
+#                Blin_neg.flnc_BC.fasta
+#                B_BM_tot.flnc.BC.fasta
+#
+#  Pseudo code:
+#             The rank files contain 
+#
+#
+#----------------------------------
+# Get the data
 cd ../data/BC_ranked_isoforms
 
+#
+# Get the Full-length PacBio Consensus Fasta Files for the samples
+#
+cp /Users/annedeslattesmays/Scitechcon\ Dropbox/Anne\ DeslattesMays/Single_cell_analysis/scRNA\ paper/single\ cell\ Long\ Read\ Protein\ Analysis/Alin_neg.flnc_BC.fasta .
+cp /Users/annedeslattesmays/Scitechcon\ Dropbox/Anne\ DeslattesMays/Single_cell_analysis/scRNA\ paper/single\ cell\ Long\ Read\ Protein\ Analysis/Blin_neg.flnc_BC.fasta .
+cp /Users/annedeslattesmays/Scitechcon\ Dropbox/Anne\ DeslattesMays/Single_cell_analysis/scRNA\ paper/single\ cell\ Long\ Read\ Protein\ Analysis/B_BM_tot.flnc_BC.fasta .
+
+#
+# Get the barcoded ranked clusters
+#
+cp /Users/annedeslattesmays/Scitechcon\ Dropbox/Anne\ DeslattesMays/Single_cell_analysis/PacBio/single_cell_pipeline/BC-ranked_isoforms/BC_ranked_isoforms/*.csv .
+
+Alin_neg_fasta="Alin_neg.flnc_BC.fasta"
+Blin_neg_fasta="Blin_neg.flnc_BC.fasta"
+B_BM_tot_fasta="B_BM_tot.flnc_BC.fasta"
+
+echo "Alin_neg_fasta    = " $Alin_neg_fasta
+echo "Blin_neg_fasta    = " $Blin_neg_fasta
+echo "B_BM_tot_fasta    = " $B_BM_tot_fasta
+
 allcsv="*.csv"
+
 
 for file in $allcsv; do
     
@@ -16,33 +52,29 @@ for file in $allcsv; do
     file_pbids="$name$pbids"
     file_ccsids="$name$ccsids"
     file_ccsids_fasta="$name$ccsfasta"
-    Alin_neg_fasta="Alin_neg.flnc_BC.fasta"
-    Blin_neg_fasta="Blin_neg.flnc_BC.fasta"
-    B_BM_tot_fasta="B_BM_tot.flnc.BC.fasta"
     
     echo "file_pbids        = " $file_pbids
     echo "file_ccsids       = " $file_ccsids
-    echo "Alin_neg_fasta    = " $Alin_neg_fasta
-    echo "Blin_neg_fasta    = " $Blin_neg_fasta
-    echo "B_BM_tot_fasta    = " $B_BM_tot_fasta
+    echo "file_ccsids_fasta = " $file_ccsids_fasta
     
     cut -f 2 $file > $file_pbids
     cut -f 1 $file > $file_ccsids
-done
 
-for file in "Alin*"; do
-    echo "extracting Alin clusters and making " $file_ccsids_fasta 
-    seqkit grep -n -f $file_ccsids $Alin_neg_fasta > $file_ccsids_fasta
-done
 
-for file in "Blin*"; do
-    echo "extracting Blin clusters and making " $file_ccsids_fasta
-    seqkit grep -n -f $file_ccsids $Blin_neg_fasta > $file_ccsids_fasta
-done
+    # in Bash - spaces are important!  without a space around the string, it interrupts as a command
+    if [[ $file_ccsids == "Alin"* ]]; then 
+	echo "extracting Alin clusters and making " $file_ccsids_fasta
+        seqkit grep -n -f $file_ccsids $Alin_neg_fasta > $file_ccsids_fasta
+    fi
+    if [[ $file_ccsids == "^Blin"* ]]; then
+	echo "extracting Blin clusters and making " $file_ccsids_fasta
+	seqkit grep -n -f $file_ccsids $Blin_neg_fasta > $file_ccsids_fasta
+    fi
+    if [[ $file_ccsids == "^B_BM_tot"* ]]; then
+	echo "extracting B_BM_tot clusters and making " $file_ccsids_fasta
+	seqkit grep -n -f $file_ccsids $B_BM_tot_fasta > $file_ccsids_fasta
+    fi
 
-for file in "B_BM_tot*"; do
-    echo "extracting B_BM_tot clusters and making " $file_ccsids_fasta
-    seqkit grep -n -f $file_ccsids $B_BM_tot_fasta > $file_ccsids_fasta
 done
 
     
