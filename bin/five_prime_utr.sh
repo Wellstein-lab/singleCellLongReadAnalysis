1e592c2f5215a1576aaa01c0a5b3910b70008a0d
Anne Deslattes Mays, PhD <adeslat@scitechcon.org>
2022-05-09 09:34:38 -0400

    initial commit

--- bin/five_prime_utr.sh
+++ bin/five_prime_utr.sh
@@ -42,7 +42,15 @@ allwithgtf="*cds.transcript_exons_only.gtf"
 echo "Current Working Directory is = " $PWD
 echo "all with transcript gtf      = " $allwithgtf
 
-# loop through 
+#
+# this is run just once - if the version of Gencode changes
+# -- TODO - move this in the data preparation step -- the reference set up step
+#  this is the command as run on the gencode.v32.primary_assembly.annotation.gtf
+#
+#  docker run -v $PWD:$PWD -w $PWD gsheynkmanlab/proteogenomics-base:v1.0 1_get_gc_exon_and_5utr_info.py --gencode_gtf gencode.v32.primary_assembly.annotation.gtf --odir ./
+
+# loop through
+#
 for file in $allwithgtf; do
     name="${file%%.*}"
     name_merge5_corrected_5degfilter_with_transcript_with_cds_exons_only_gtf=$name$merge5$corrected$degfilter$with_transcript_with_cds$transcript_exons_only$gtf
@@ -56,6 +64,8 @@ for file in $allwithgtf; do
     echo "name_merge5_corrected_5degfilter_with_transcript_with_cds_renamed_exon_gtf = " $name_merge5_corrected_5degfilter_with_transcript_with_cds_renamed_exon_gtf
     echo "name_merge5_corrected_5degfilter_best_orf                                  = " $name_merge5_corrected_5degfilter_best_orf
     
+    docker run -v $PWD:$PWD -w $PWD gsheynkmanlab/proteogenomics-base:v1.0 2_classify_5utr_status.py \
+	   --gencode_exons_bed gencode_exons_for_cds_containing_ensts.bed --gencode_exons_chain gc_exon_chain_strings_for_cds_containing_transcripts.tsv --sample_cds_gtf filtered_Blin_neg_filt_ranked_BC_clust0_B_ccsids.merge5.collapsed_corrected.5degfilter_with_transcript_with_cds.gtf --odir ./
     # variables need to be provided in a specific order
     docker run -v $PWD:$PWD -w $PWD gsheynkmanlab/sqanti_protein:sing sqanti3_protein.py \
 	   $name_merge5_corrected_5degfilter_with_transcript_with_cds_exons_only_gtf \
